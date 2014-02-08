d4045352c2dd6de4b197bd99a79ecdf81dcccff2
Ingvar Stepanyan <me@rreverser.com>
2014-02-08 16:29:58 +0200

    jBinary.load: switch->case to if-else-... linear chain until mishoo/UglifyJS2#376 is fixed. Updated `npm test`.

--- src/io/load.js
+++ src/io/load.js
@@ -3,113 +3,105 @@ var ReadableStream = NODE && require('stream').Readable;
 jBinary.loadData = promising(function (source, callback) {
 	var dataParts;
 
-	switch (true) {
-		case BROWSER && 'Blob' in global && source instanceof Blob:
-			var reader = new FileReader();
-			reader.onload = reader.onerror = function () { callback(this.error, this.result) };
-			reader.readAsArrayBuffer(source);
-			return;
+	if (BROWSER && 'Blob' in global && source instanceof Blob) {
+		var reader = new FileReader();
+		reader.onload = reader.onerror = function () { callback(this.error, this.result) };
+		reader.readAsArrayBuffer(source);
+	} else
+	if (NODE && ReadableStream && source instanceof ReadableStream) {
+		var buffers = [];
+		source
+			.on('readable', function () { buffers.push(this.read()) })
+			.on('end', function () { callback(null, Buffer.concat(buffers)) })
+			.on('error', callback)
+		;
+	} else
+	if (typeof source !== 'string') {
+		return callback(new TypeError('Unsupported source type.'));
+	} else
+	if (!!(dataParts = source.match(/^data:(.+?)(;base64)?,(.*)$/))) {
+		try {
+			var isBase64 = dataParts[2],
+				content = dataParts[3];
 
-		case NODE && !!ReadableStream && source instanceof ReadableStream:
-			var buffers = [];
-			source
-				.on('readable', function () { buffers.push(this.read()) })
-				.on('end', function () { callback(null, Buffer.concat(buffers)) })
-				.on('error', callback)
-			;
-			return;
+			callback(
+				null,
+				(
+					(isBase64 && NODE && jDataView.prototype.compatibility.NodeBuffer)
+					? new Buffer(content, 'base64')
+					: (isBase64 ? atob : decodeURIComponent)(content)
+				)
+			);
+		} catch (e) {
+			callback(e);
+		}
+	} else
+	if (BROWSER && 'XMLHttpRequest' in global) {
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', source, true);
 
-		case typeof source !== 'string':
-			return callback(new TypeError('Unsupported source type.'));
+		// new browsers (XMLHttpRequest2-compliant)
+		if ('responseType' in xhr) {
+			xhr.responseType = 'arraybuffer';
+		}
+		// old browsers (XMLHttpRequest-compliant)
+		else if ('overrideMimeType' in xhr) {
+			xhr.overrideMimeType('text/plain; charset=x-user-defined');
+		}
+		// IE9 (Microsoft.XMLHTTP-compliant)
+		else {
+			xhr.setRequestHeader('Accept-Charset', 'x-user-defined');
+		}
 
-		case !!(dataParts = source.match(/^data:(.+?)(;base64)?,(.*)$/)):
-			try {
-				var isBase64 = dataParts[2],
-					content = dataParts[3];
-
-				callback(
-					null,
-					(
-						(isBase64 && NODE && jDataView.prototype.compatibility.NodeBuffer)
-						? new Buffer(content, 'base64')
-						: (isBase64 ? atob : decodeURIComponent)(content)
-					)
-				);
-			} catch (e) {
-				callback(e);
-			}
-
-			return;
-
-		case BROWSER && 'XMLHttpRequest' in global:
-			var xhr = new XMLHttpRequest();
-			xhr.open('GET', source, true);
-
-			// new browsers (XMLHttpRequest2-compliant)
-			if ('responseType' in xhr) {
-				xhr.responseType = 'arraybuffer';
-			}
-			// old browsers (XMLHttpRequest-compliant)
-			else if ('overrideMimeType' in xhr) {
-				xhr.overrideMimeType('text/plain; charset=x-user-defined');
-			}
-			// IE9 (Microsoft.XMLHTTP-compliant)
-			else {
-				xhr.setRequestHeader('Accept-Charset', 'x-user-defined');
-			}
-
-			// shim for onload for old IE
-			if (!('onload' in xhr)) {
-				xhr.onreadystatechange = function () {
-					if (this.readyState === 4) {
-						this.onload();
-					}
-				};
-			}
-
-			var cbError = function (string) {
-				callback(new Error(string));
-			};
-
-			xhr.onload = function () {
-				if (this.status !== 0 && this.status !== 200) {
-					return cbError('HTTP Error #' + this.status + ': ' + this.statusText);
+		// shim for onload for old IE
+		if (!('onload' in xhr)) {
+			xhr.onreadystatechange = function () {
+				if (this.readyState === 4) {
+					this.onload();
 				}
-
-				// emulating response field for IE9
-				if (!('response' in this)) {
-					this.response = new VBArray(this.responseBody).toArray();
-				}
-
-				callback(null, this.response);
 			};
+		}
 
-			xhr.onerror = function () {
-				cbError('Network error.');
-			};
+		var cbError = function (string) {
+			callback(new Error(string));
+		};
 
-			xhr.send(null);
+		xhr.onload = function () {
+			if (this.status !== 0 && this.status !== 200) {
+				return cbError('HTTP Error #' + this.status + ': ' + this.statusText);
+			}
 
-			return;
+			// emulating response field for IE9
+			if (!('response' in this)) {
+				this.response = new VBArray(this.responseBody).toArray();
+			}
 
-		case BROWSER:
-			return callback(new TypeError('Unsupported source type.'));
+			callback(null, this.response);
+		};
 
-		case NODE && /^(https?):\/\//.test(source):
-			require('request').get({
-				uri: source,
-				encoding: null
-			}, function (error, response, body) {
-				if (!error && response.statusCode !== 200) {
-					var statusText = require('http').STATUS_CODES[response.statusCode];
-					error = new Error('HTTP Error #' + response.statusCode + ': ' + statusText);
-				}
-				callback(error, body);
-			});
-			return;
+		xhr.onerror = function () {
+			cbError('Network error.');
+		};
 
-		case NODE:
-			require('fs').readFile(source, callback);
+		xhr.send(null);
+	} else
+	if (BROWSER) {
+		return callback(new TypeError('Unsupported source type.'));
+	} else
+	if (NODE && /^(https?):\/\//.test(source)) {
+		require('request').get({
+			uri: source,
+			encoding: null
+		}, function (error, response, body) {
+			if (!error && response.statusCode !== 200) {
+				var statusText = require('http').STATUS_CODES[response.statusCode];
+				error = new Error('HTTP Error #' + response.statusCode + ': ' + statusText);
+			}
+			callback(error, body);
+		});
+	} else
+	if (NODE) {
+		require('fs').readFile(source, callback);
 	}
 });
 
