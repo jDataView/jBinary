fc8fa034528ac4000bba314b843740c57a53289f
Ingvar Stepanyan <me@rreverser.com>
2014-04-13 04:13:04 +0300

    Fix sources protocol to HTTPS.

--- grunt/concat_sourcemap.js
+++ grunt/concat_sourcemap.js
@@ -2,7 +2,7 @@ module.exports = function (grunt) {
 	return {
 		options: {
 			separator: Array(3).join(grunt.util.linefeed),
-			sourceRoot: process.env.CI ? 'http://raw.github.com/' + process.env.TRAVIS_REPO_SLUG + '/' + process.env.TRAVIS_COMMIT : '../..'
+			sourceRoot: process.env.CI ? 'https://raw.github.com/' + process.env.TRAVIS_REPO_SLUG + '/' + process.env.TRAVIS_COMMIT : '../..'
 		},
 		all: {
 			files: {
