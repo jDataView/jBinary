da8c91153b1eb24b14927ad8b49f4ebf5465340d
Ingvar Stepanyan <me@rreverser.com>
2014-04-14 01:40:36 +0300

    Include Firefox for CI testing.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -4,7 +4,7 @@ module.exports = function (config) {
 	config.set({
 		basePath: '..',
 		frameworks: ['mocha', 'chai'],
-		browsers: CI ? ['PhantomJS'] : ['Chrome', 'Firefox', 'IE'],
+		browsers: CI ? ['PhantomJS', 'Firefox'] : ['Chrome', 'Firefox', 'IE'],
 		files: [
 			'http://jdataview.github.io/dist/jdataview.js',
 			'dist/browser/jbinary.js',
