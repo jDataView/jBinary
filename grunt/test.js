0b084e6e1bafe060dfb2c8a5b58114e4e85c1174
Ingvar Stepanyan <me@rreverser.com>
2014-04-29 01:42:43 +0300

    Faster builds by using grunt-newer; `grunt clean` needs to be executed explicitly from now.

--- grunt/test.js
+++ grunt/test.js
@@ -1,10 +1,20 @@
 module.exports = function (grunt) {
 	grunt.registerMultiTask('test', function () {
-		grunt.task.run(this.data + ':' + this.target);
+		grunt.task.run(this.options().task + ':' + this.target);
 	});
 
 	return {
-		browser: 'karma',
-		node: 'mochaTest'
+		browser: {
+			options: {
+				task: 'karma'
+			},
+			src: 'dist/browser/<%= pkgName %>.js'
+		},
+		node: {
+			options: {
+				task: 'mochaTest'
+			},
+			src: 'dist/node/<%= pkgName %>.js'
+		}
 	};
 };
